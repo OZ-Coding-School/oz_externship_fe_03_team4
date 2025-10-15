1874b4bfaf04218c74be215b9aa6fb122c4af886
danbi-seo <sdb0504@naver.com>
2025-10-16 02:31:05 +0900

    💡 chore : 와이어프레임에 맞게 css 정리(#6)

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,19 +4,24 @@ interface BadgeProps {
   label: string
 }
 
-const Badge = ({ className = '', variant = 'default', label }: BadgeProps) => {
+export const Badge = ({
+  className = '',
+  variant = 'default',
+  label,
+}: BadgeProps) => {
   const variantStyles = {
-    default: 'bg-[#f3f4f6 ]text-[#1F2937]',
-    primary: 'bg-[#DBEAFE] text-[#1E40AF]]',
-    success: 'bg-[#DCFCE7]] text-[#166534]',
+    default: 'bg-[#f3f4f6] text-[#1F2937]',
+    primary: 'bg-[#DBEAFE] text-[#1E40AF]',
+    success: 'bg-[#DCFCE7] text-[#166534]',
     warning: 'bg-[#FEF9C3] text-[#854D0E]',
     danger: 'bg-[#FEE2E2] text-[#991B1B]',
     info: 'bg-[#F3E8FF] text-[#6B21A8]',
   }
 
+  //cursor-default:클릭 비활성화 / px-2 (8px) py-1(4px) text-xs(12px)
   return (
     <button
-      className={`rounded-full px-6 py-3 text-lg font-semibold ${variantStyles[variant]} ${className} `}
+      className={`cursor-default rounded-full px-2 py-1 text-xs font-medium ${variantStyles[variant]} ${className} `}
       type="button"
     >
       {label}
@@ -25,18 +30,15 @@ const Badge = ({ className = '', variant = 'default', label }: BadgeProps) => {
 }
 
 // 사용 테스트
-const BadgeTest = () => {
-  return (
-    <div className="mb-12 flex flex-wrap gap-4">
-      <Badge variant="default" label="Default" />
-      <Badge variant="primary" label="Primary" />
-      <Badge variant="success" label="Success" />
-      <Badge variant="warning" label="Warning" />
-      <Badge variant="danger" label="Danger" />
-      <Badge variant="info" label="Info" />
-    </div>
-  )
-}
-
-export default BadgeTest
-export { Badge }
+// export const BadgeTest = () => {
+//   return (
+//     <div className="mb-12 flex flex-wrap gap-4">
+//       <Badge variant="default" label="Default" />
+//       <Badge variant="primary" label="Primary" />
+//       <Badge variant="success" label="Success" />
+//       <Badge variant="warning" label="Warning" />
+//       <Badge variant="danger" label="Danger" />
+//       <Badge variant="info" label="Info" />
+//     </div>
+//   )
+// }
