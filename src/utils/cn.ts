648bbd58672223ebfca6457005151d91674a26da
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-16 12:45:29 +0900

    ♻️ refactor : clsx추가적용 및 예제페이지 주석작성

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,7 +2,8 @@
 // 클래스가 많은 곳일수록 빛을 발하는 친구입니다.
 
 import { twMerge } from 'tailwind-merge'
+import clsx from 'clsx'
 
-export const cn = (...classNames: Array<string | false | null | undefined>) => {
-  return twMerge(classNames.filter(Boolean).join(' '))
+export const cn = (...inputs: Array<string | undefined | null | false>) => {
+  return twMerge(clsx(inputs))
 }
