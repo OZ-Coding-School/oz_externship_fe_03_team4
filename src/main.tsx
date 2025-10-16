491d1779b8499cce0b8606975c5b1ec51de810c3
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-16 13:18:23 +0900

    🐛 fix : main.tsx 내용 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,10 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 
 // import './index.css'
-import ExampleForm from './components/FormUI/ExampleForm'
-// import App from './App.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ExampleForm />
+    <App />
   </StrictMode>
 )
