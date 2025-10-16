faa543ce0de046cd46deb706665c2813c0808b02
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-16 14:11:39 +0900

    💡 chore : 오타부분 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<ButtonTestPage />} />
-          {/* <Route index element={<ButtonTestPage />} /> 
+          {/* <Route path="users" element={<userspage />} /> 
             아래 부분에 이렇게 계속 라우팅 추가해주시면 됩니다. */}
         </Route>
       </Routes>
