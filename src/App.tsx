17b18ba9f8f44838b5bf5653028d601997a85185
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-17 19:20:21 +0900

    bug: 테스트페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './App.css'
 import Layout from './components/Layout'
-import ButtonTestPage from './pages/tests/ButtonTestPage'
-import { LocalFilterExample } from './components/search/LocalFilterExample'
-import ExampleForm from './components/FormUI/ExampleForm'
-import AdminLoginPage from './pages/AdminLoginPage'
 import DanbiTestPage from './pages/tests/DanbiTestPage'
 import HYTestPage from './pages/tests/HYTestPage'
 import WonheeTestPage from './pages/tests/WonheeTestPage'
@@ -15,12 +11,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="login" element={<AdminLoginPage />} />
         <Route element={<Layout />}>
           {/* <Route index element={<ToastTest />} /> */}
-          <Route index element={<ButtonTestPage />} />
-          <Route path="searchtest" element={<LocalFilterExample />} />
-          <Route path="example" element={<ExampleForm />} />
           <Route path="wonhee" element={<WonheeTestPage />} />
           <Route path="danbi" element={<DanbiTestPage />} />
           <Route path="hy" element={<HYTestPage />} />
