5304eb4bfc571ab4a362841b2e166ad85c6dda7f
djskyt <jjsk953563@gmail.com>
2025-10-17 16:25:53 +0900

    🐛 ♻️ refactor : App.tsx 잘못 만진거 다시 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './App.css'
 import Layout from './components/Layout'
-import ButtonTestPage from './pages/tests/ButtonTestPage'
-import { LocalFilterExample } from './components/search/LocalFilterExample'
-import ExampleForm from './components/FormUI/ExampleForm'
 
 function App() {
   return (
@@ -11,9 +8,6 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           {/* <Route index element={<ToastTest />} /> */}
-          <Route index element={<ButtonTestPage />} />
-          <Route path="searchtest" element={<LocalFilterExample />} />
-          <Route path="example" element={<ExampleForm />} />
           {/* <Route path="users" element={<userspage />} /> 
             아래 부분에 이렇게 계속 라우팅 추가해주시면 됩니다. */}
         </Route>
@@ -21,4 +15,5 @@ function App() {
     </Router>
   )
 }
+
 export default App
