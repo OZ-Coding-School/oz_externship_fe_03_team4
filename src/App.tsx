18c02c05dcbcb3b6157f09d15bc655a5be784b02
paul8729621 <paul872@naver.com>
2025-10-17 11:04:59 +0900

    feat(Routeing):테스트 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,21 @@ import Layout from './components/Layout'
 import DanbiTestPage from './pages/tests/DanbiTestPage'
 import HYTestPage from './pages/tests/HYTestPage'
 import WonheeTestPage from './pages/tests/WonheeTestPage'
-import KyungbokTestPage from './pages/tests/kyungbokTestPage'
-import HyunjinTestPage from './pages/tests/hyunjinTestPage'
+import KyungbokTestPage from './pages/tests/KyungbokTestPage'
+import HyunjinTestPage from './pages/tests/HyunjinTestPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          {/* <Route index element={<ToastTest />} /> */}
-          <Route path="wonhee" element={<WonheeTestPage />} />
-          <Route path="danbi" element={<DanbiTestPage />} />
-          <Route path="hy" element={<HYTestPage />} />
-          <Route path="bok" element={<KyungbokTestPage />} />
-          <Route path="hyunjin" element={<HyunjinTestPage />} />
-          {/* <Route path="users" element={<userspage />} /> 
+        <Route path="danbi" element={<DanbiTestPage />} />
+        <Route path="wonhee" element={<WonheeTestPage />} />
+        <Route path="hy" element={<HYTestPage />} />
+        <Route path="hyunjin" element={<HyunjinTestPage />} />
+        <Route path="bok" element={<KyungbokTestPage />} />
+          {/* <Route index element={<ToastTest />} /> /}
+          {/ <Route path="users" element={<userspage />} /> 
             아래 부분에 이렇게 계속 라우팅 추가해주시면 됩니다. */}
         </Route>
       </Routes>
