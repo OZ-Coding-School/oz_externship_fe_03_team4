39fb6caecad127a2dceb67a569bcdb31bf63aba2
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-17 15:22:57 +0900

    ✨ feat : 로그인페이지 대략적인 로직 제작완료

--- src/pages/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage.tsx
@@ -7,5 +7,27 @@ import { FormField, TextField } from '../components/FormUI'
 export default function AdminLoginPage() {
   const [showPassword, setShowPassword] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const [emailError, setEmailError] = useState<string | undefined>()
+  const [pwError, setPwError] = useState<string | undefined>()
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setEmailError(undefined)
+    setPwError(undefined);
+
+    try {
+        // await api.post("v1/auth/login", {email, password});
+    } catch (err) {
+        console.error(err);
+    } finally {
+        setIsLoading(false);
+    }
+};
+
+    return(
+        <main className="min-h-screen bg-white"
+    )
+    }
+  }
 }
