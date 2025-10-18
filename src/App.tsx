23bd42cf44830d35bfadb3cd2f5459a7b7cf6013
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-18 11:45:15 +0900

    💡 chore : 관리자페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import HYTestPage from './pages/tests/HYTestPage'
 import WonheeTestPage from './pages/tests/WonheeTestPage'
 import KyungbokTestPage from './pages/tests/KyungbokTestPage'
 import HyunjinTestPage from './pages/tests/HyunjinTestPage'
+import AdminLoginPage from './pages/AdminLoginPage'
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 완성된 페이지 추가 */}
+        <Route path="/" element={<AdminLoginPage />} />
+
         <Route element={<Layout />}>
-        <Route path="danbi" element={<DanbiTestPage />} />
-        <Route path="wonhee" element={<WonheeTestPage />} />
-        <Route path="hy" element={<HYTestPage />} />
-        <Route path="hyunjin" element={<HyunjinTestPage />} />
-        <Route path="bok" element={<KyungbokTestPage />} />
+          <Route path="danbi" element={<DanbiTestPage />} />
+          <Route path="wonhee" element={<WonheeTestPage />} />
+          <Route path="hy" element={<HYTestPage />} />
+          <Route path="hyunjin" element={<HyunjinTestPage />} />
+          <Route path="bok" element={<KyungbokTestPage />} />
           {/* <Route index element={<ToastTest />} /> /}
           {/ <Route path="users" element={<userspage />} /> 
             아래 부분에 이렇게 계속 라우팅 추가해주시면 됩니다. */}
