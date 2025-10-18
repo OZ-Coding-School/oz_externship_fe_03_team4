17daa4edd771e3dc97a88e11c97911e4ae512b58
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-18 10:13:15 +0900

    ✨ feat : protected router 제작시작

---
+++ src/lib/axios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  withCredentials: true,
+  timeout: 10000,
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+export default api
