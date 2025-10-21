d3df0a8d6c615194e0bab617c62120a5d0282268
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-21 13:57:34 +0900

    💡 chore : token.ts 파일 내 오타수정

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie'
 
 export const setAccessToken = (token: string) => {
-  Cookies.set('accessToken', token, { expries: 1 }) // 만료일은 우선 "1일"로 지정하며 추후 변경가능함.
+  Cookies.set('accessToken', token, { expires: 1 }) // 만료일은 우선 "1일"로 지정하며 추후 변경가능함.
 }
 
 export const getAccessToken = (): string | null => {
