24e17134707cb99fbf4a0219c3e1dfc9e00afaab
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-21 19:00:50 +0900

    ✨ feat : useReviewsQuery 완성

---
+++ src/hooks/reviews/useReviewsQuery.ts
@@ -0,0 +1,28 @@
+import {
+  useQuery,
+  keepPreviousData,
+  type UseQueryResult,
+} from '@tanstack/react-query'
+import { adminReviewQueryKey } from './queryKeys'
+import { buildQueryParams } from './buildQueryParams'
+import { fetchReviews, type FetchReviewsReturn } from './fetchReviews'
+import type { ReviewsParams } from './types.local'
+
+export const useReviewsQuery = (
+  reviewsParams: ReviewsParams
+): UseQueryResult<FetchReviewsReturn, Error> => {
+  const queryParams = buildQueryParams(reviewsParams)
+  const fetchAdminReviewList = () => fetchReviews(queryParams)
+
+  return useQuery<
+    FetchReviewsReturn,
+    Error,
+    FetchReviewsReturn,
+    ReturnType<typeof adminReviewQueryKey>
+  >({
+    queryKey: adminReviewQueryKey(queryParams),
+    queryFn: fetchAdminReviewList,
+    placeholderData: keepPreviousData,
+    staleTime: 30_000,
+  })
+}
