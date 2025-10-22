bf2f0ba312a2ce5542e40ae127d6eec2d19ee34d
paul8729621 <paul872@naver.com>
2025-10-22 18:05:58 +0900

    style(UserListPage):권한 변경하기 버튼의 위치 수정

--- src/pages/UserListPage.tsx
+++ src/pages/UserListPage.tsx
@@ -221,10 +221,15 @@ const UserListPage = () => {
                 <div>탈퇴요청일: {selectedUser.withdrawAt}</div>
               </div>
 
-              <div className="flex justify-end gap-2 mt-4">
+              <div className="flex justify-between items-center mt-4">
+                {/* 왼쪽: 권한 변경하기 */}
                 <Button color="success" size="medium">권한 변경하기</Button>
-                <Button color="primary" size="medium">수정하기</Button>
-                <Button color="danger" size="medium">삭제하기</Button>
+              
+                {/* 오른쪽: 수정 / 삭제 */}
+                <div className="flex gap-2">
+                  <Button color="primary" size="medium">수정하기</Button>
+                  <Button color="danger" size="medium">삭제하기</Button>
+                </div>
               </div>
             </div>
           </Modal>
