383835ca8cd040ec1461f9542beb07181b3e501c
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-23 20:14:37 +0900

    ✨ feat : 일정순 추가

---
+++ src/components/application/filter/SortSelect.tsx
@@ -0,0 +1,18 @@
+import { Select } from '../../FormUI'
+import type { SortKey } from '../../../types/applications'
+
+interface SortSelectProps {
+  value: SortKey
+  onChange: (v: SortKey) => void
+}
+
+export const SortSelect = ({ value, onChange }: SortSelectProps) => {
+  return (
+    <Select value={value} onChange={(e) => onChange(e.target.value as SortKey)}>
+      <option value="-appliedAt">지원일 최신순</option>
+      <option value="appliedAt">지원일 오래된순</option>
+      <option value="-updatedAt">수정일 최신순</option>
+      <option value="updatedAt">수저일 오래된순</option>
+    </Select>
+  )
+}
