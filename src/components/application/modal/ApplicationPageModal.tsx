f2fc37cd32d9be2d2b563b1f106d7748a15602e4
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-24 20:42:28 +0900

    feat: 상세모달 완성

--- src/components/application/modal/ApplicationPageModal.tsx
+++ src/components/application/modal/ApplicationPageModal.tsx
@@ -1,5 +1,29 @@
+import type { ApplicationDetail } from '../../../types/applications'
 import { CloseModalFooter } from '../../modal/CloseModalFooter'
 import Modal from '../../modal/Modal'
 import { ModalHeader } from '../../modal/ModalHeader'
 import { ApplicationModalOutlet } from './ApplicationModalOutlet'
 
+interface ApplicationPageModalProps {
+  open: boolean
+  onClose: () => void
+  detail: ApplicationDetail | null
+}
+
+export const ApplicationPageModal = ({
+  open,
+  onClose,
+  detail,
+}: ApplicationPageModalProps) => {
+  if (!detail) return null
+
+  return (
+    <Modal isOn={open} onBackgroundClick={onClose}>
+      <ModalHeader title="지원 내역 상세" onClose={onClose} />
+      <div className="max-h-[75vh] overflow-y-auto px-6 py-4">
+        <ApplicationModalOutlet detail={detail} />
+      </div>
+      <CloseModalFooter onClose={onClose} />
+    </Modal>
+  )
+}
