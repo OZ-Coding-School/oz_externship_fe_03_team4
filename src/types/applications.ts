818072864756e2203a652bb261516e4b5b5f3005
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-24 14:38:08 +0900

    chore: 지원상태 변경

--- src/types/applications.ts
+++ src/types/applications.ts
@@ -1,10 +1,10 @@
 // API 스키마
 // 지원 상태 (서버에서 내려오는 값)
 export type AdminApplicationStatus =
-  | 'APPROVED'
-  | 'APPLIED'
-  | 'PENDING'
-  | 'REJECTED'
+  | 'ACCEPTED' // 승인
+  | 'APPLYING' // 지원중
+  | 'REVIEWING' // 검토중
+  | 'REJECTED' // 거절
 // 서버에서 제공해주는 지원 내역 데이터들
 export interface AdminApplicationApi {
   id: number // 고유id
@@ -23,7 +23,7 @@ export type AdminSortKey =
   | 'updated_at'
 
 // ui용
-export type ApplicationStatus = '승인' | '검토중' | '대기' | '거절'
+export type ApplicationStatus = '승인' | '지원중' | '검토중' | '거절'
 export type StatusFilter = '전체' | ApplicationStatus // 상태 필터 옵션
 // 지원자 정보
 export interface Applicant {
@@ -41,19 +41,21 @@ export interface Application {
 }
 
 // 상태/라벨 매핑하기
-export const apiStatusToUi: Record<AdminApplicationStatus, ApplicationStatus> =   // api -> 상태
+export const apiStatusToUi: Record<AdminApplicationStatus, ApplicationStatus> =
+  // api -> 상태
   {
-    APPROVED: '승인',
-    APPLIED: '검토중',
-    PENDING: '대기',
+    ACCEPTED: '승인',
+    APPLYING: '지원중',
+    REVIEWING: '검토중',
     REJECTED: '거절',
   }
 
-export const uiStatusToApi: Record<ApplicationStatus, AdminApplicationStatus> =   // 요긴 반대입니당
+export const uiStatusToApi: Record<ApplicationStatus, AdminApplicationStatus> =
+  // 요긴 반대입니당
   {
-    승인: 'APPROVED',
-    검토중: 'APPLIED',
-    대기: 'PENDING',
+    승인: 'ACCEPTED',
+    지원중: 'APPLYING',
+    검토중: 'REVIEWING',
     거절: 'REJECTED',
   }
 
