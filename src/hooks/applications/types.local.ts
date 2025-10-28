8f700f4b1ef90a0ed39ff1f0abf5628b0ac2f29b
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-28 13:58:05 +0900

    💡 chore : 주석 추가

---
+++ src/hooks/applications/types.local.ts
@@ -0,0 +1,7 @@
+export type ApplicationsParams = {
+  searchText?: string       // 검색어
+  statusFilter?: '전체' | '승인' | '검토중' | '대기' | '거절'   // 필터 조건
+  pageNumber?: number       // 현재 페이지 번호
+  pageSize: number      // 한 페이지에 보여줄 목록의 개수
+  sortKey: '-created_at' | 'created_at' | '-updated_at' | 'updated_at'      // 정렬 기준
+}
