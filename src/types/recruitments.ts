ab11c336a2b9d1e58a04579eda360503dbc76252
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-29 11:18:19 +0900

    ✨ feat : 목록 응답값 정의

--- src/types/recruitments.ts
+++ src/types/recruitments.ts
@@ -1,15 +1,23 @@
-export type RecruitmentStatusApi = '모집중' | '마감'    // API에서 내려오는 값
-export type RecruitmentOrderingApi = 'latest' | 'oldest' | 'views' | 'bookmarks'    // 정렬 키값
+export type RecruitmentStatusApi = '모집중' | '마감' // API에서 내려오는 값
+export type RecruitmentOrderingApi = 'latest' | 'oldest' | 'views' | 'bookmarks' // 정렬 키값
 
 // 서버 응답
 export interface RecruitmentDTO {
-    id: number
-    title: string
-    tags: string[]
-    close_at: string
-    status: RecruitmentStatusApi
-    views_count: number
-    bookmarks_count: number
-    created_at: string
-    updated_at: string | null
+  id: number
+  title: string
+  tags: string[]
+  close_at: string
+  status: RecruitmentStatusApi
+  views_count: number
+  bookmarks_count: number
+  created_at: string
+  updated_at: string | null
 }
+
+export interface RecruitmentListResponseDTO {
+  results: RecruitmentDTO[]
+  page: number
+  page_size: number
+  total_count: number
+}
+
