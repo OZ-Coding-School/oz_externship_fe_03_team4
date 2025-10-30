15e47d3b87d6c66cc0332c3002db609fea684381
kyeongbok <kyeongbok_0627@kakao.com>
2025-10-30 20:00:11 +0900

    style: modal 스타일 변경

--- src/components/login/ErrorModal.tsx
+++ src/components/login/ErrorModal.tsx
@@ -1,3 +1,4 @@
+import { AlertTriangle } from 'lucide-react'
 import Modal from '../modal/Modal'
 import RoundBox from '../modal/Roundbox'
 
@@ -16,8 +17,20 @@ export const LoginErrorModal = ({
 
   return (
     <Modal isOn={isOn} onBackgroundClick={onClose}>
-      <RoundBox padding="md" className="w-[360px] space-y-4 text-center">
-        <p className="text-sm text-neutral-800">{message}</p>
+      <RoundBox
+        padding="md"
+        className="h-[230px] w-[360px] space-y-4 text-center"
+      >
+        <div className="flex justify-center">
+          <AlertTriangle
+            className="text-amber-500"
+            size={80}
+            strokeWidth={2.2}
+          />
+        </div>
+        <p className="text-lg whitespace-pre-line text-neutral-800">
+          {message}
+        </p>
         <button
           onClick={onClose}
           className="rounded-md bg-amber-500 px-4 py-2 text-sm font-medium text-white"
