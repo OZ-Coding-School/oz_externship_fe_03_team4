323749e10a97922d2d8347e7fd30b0997fd9e657
kyeongbok <kyeongbok_0627@kakao.com>
2025-11-01 00:16:01 +0900

    fix: 모달푸터 프롭스로 받을 수 있도록 수정

--- src/components/recruitments/modal/RecruitmentModal.tsx
+++ src/components/recruitments/modal/RecruitmentModal.tsx
@@ -2,7 +2,7 @@ import Modal from '../../modal/Modal'
 import type { RecruitmentDetail } from '../../../types/recruitments'
 import { ModalHeader } from '../../modal/ModalHeader'
 import { RecruitmentModalOutlet } from './RecruitmentModalOutlet'
-import { RecruitmentModalFooter } from './RecruitmentModalFooter'
+import { CloseModalFooter } from '../../modal/CloseModalFooter'
 
 interface RecruitmentModalProps {
   open: boolean
@@ -21,7 +21,7 @@ export const RecruitmentModal = ({
     <Modal isOn={open} onBackgroundClick={onClose}>
       <ModalHeader title="스터디 구인 공고 상세" onClose={onClose} />
       <RecruitmentModalOutlet detail={detail} />
-      <RecruitmentModalFooter onClose={onClose} onDelete={onDelete} />
+      <CloseModalFooter onClose={onClose} showDelete onDelete={onDelete} />
     </Modal>
   )
 }
