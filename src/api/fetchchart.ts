bb359bdb88312f1f7f075b61ad41811ebe7cad45
djskyt <jjsk953563@gmail.com>
2025-11-03 01:00:06 +0900

    ✨ feat : 탈퇴사유추적 api연결준비

--- src/api/fetchchart.ts
+++ src/api/fetchchart.ts
@@ -2,6 +2,7 @@ import api from '../lib/axios';
 import type { SignupStatisticsDTO } from '../types/Chart/SignupChart/types';
 import type { WithdrawalStatisticsDTO } from '../types/Chart/WithdrawChart/types';
 import type { WithdrawalReasonDistributionDTO } from '../types/Chart/WithdrawReasondoughnutChart/types';
+import type { WithdrawalReasonTrendDTO } from '../types/Chart/WithdrawReasonstick/types';
 
 export const fetchSignupStatistics = async (interval: 'month' | 'year'): Promise<SignupStatisticsDTO> => {
   const response = await api.get('/api/v1/users/statistics/signups', {
@@ -19,5 +20,11 @@ export const fetchWithdrawalStatistics = async (interval: 'month' | 'year'): Pro
 
 export const fetchWithdrawalReasons = async (): Promise<WithdrawalReasonDistributionDTO> => {
   const response = await api.get('/api/v1/admin/dashboard/withdrawal-reasons');
+  return response.data;
+};
+
+// 탈퇴 사유추세 조회 추가
+export const fetchWithdrawalReasonTrend = async (reason: string): Promise<WithdrawalReasonTrendDTO> => {
+  const response = await api.get(`/api/v1/admin/dashboard/withdrawal-reasons/${reason}`);
   return response.data;
 };
