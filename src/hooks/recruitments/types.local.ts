b89ddb1d7077ee48b0de382156b385b8a2cbcfba
kyeongbok <kyeongbok_0627@kakao.com>
2025-11-04 17:49:11 +0900

    💡 chore : 주석작성

--- src/hooks/recruitments/types.local.ts
+++ src/hooks/recruitments/types.local.ts
@@ -1,8 +1,8 @@
 export type AdminRecruitmentsParams = {
-  searchText?: string
-  statusFilter?: '모집중' | '마감'
-  selectedTags?: string[]
-  ordering: 'latest' | 'oldest' | 'views' | 'bookmarks'
-  pageNumber?: number
-  pageSize?: number
+  searchText?: string // 검색어
+  statusFilter?: '모집중' | '마감' // 상태 필터
+  selectedTags?: string[] // 태그 필터
+  ordering: 'latest' | 'oldest' | 'views' | 'bookmarks' // 정렬 방식
+  pageNumber?: number // 페이지 번호
+  pageSize?: number // 페이지당 출력될 항목의 수
 }
--- src/hooks/recruitments/useRecruitmentsQuery.ts
+++ src/hooks/recruitments/useRecruitmentsQuery.ts
@@ -1,5 +1,5 @@
 import {
-  keepPreviousData,
+  keepPreviousData, // 페이지 이동할때 이전 데이터를 유지해줍니닷.
   useQuery,
   type UseQueryResult,
 } from '@tanstack/react-query'
@@ -12,13 +12,13 @@ import {
 import type { AdminRecruitmentsParams } from './types.local'
 
 const SECOND = 1_000
-const STALE_TIME_RECRUITMENTS = 30 * SECOND
-
+const STALE_TIME_RECRUITMENTS = 30 * SECOND // 30초 동안 캐시 데이터를 사용합나디
+// 공고 데이터 요청 + 캐싱 + 로딩 및 에러 상태까지 관리해주는 훅
 export const useAdminRecruitmentsQuery = (
   options: AdminRecruitmentsParams
 ): UseQueryResult<FetchRecruitmentsReturn, Error> => {
-  const requestParams = buildRecruitmentsQueryParams(options)
-  const queryFn = () => fetchRecruitments(requestParams)
+  const requestParams = buildRecruitmentsQueryParams(options) // 서버 파라미터 변환
+  const queryFn = () => fetchRecruitments(requestParams) // 실제 api호출
 
   return useQuery<
     FetchRecruitmentsReturn,
@@ -26,9 +26,9 @@ export const useAdminRecruitmentsQuery = (
     FetchRecruitmentsReturn,
     ReturnType<typeof adminRecruitmentsQueryKey>
   >({
-    queryKey: adminRecruitmentsQueryKey(requestParams),
+    queryKey: adminRecruitmentsQueryKey(requestParams), // 캐싱 및 리패치 기준
     queryFn,
-    placeholderData: keepPreviousData,
-    staleTime: STALE_TIME_RECRUITMENTS,
+    placeholderData: keepPreviousData, // 페이지 변경해도 UI 깜빡임을 방지해줌
+    staleTime: STALE_TIME_RECRUITMENTS, // 재요청을 최소화함.
   })
 }
