84b2d8417ab4f503355c009f43f3b1f640c2f59b
paul8729621 <paul872@naver.com>
2025-11-05 17:56:08 +0900

    feat(useUpdateUser):api 연결 파일 제작

--- src/hooks/UserList/useUpdateUser.ts
+++ src/hooks/UserList/useUpdateUser.ts
@@ -2,21 +2,19 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../../lib/axios";
 import type { MappedUser } from "../../types/user";
 
-// PATCH 요청 함수
-async function updateUser(userId: number | string, updatedData: Partial<MappedUser>) {
-  const response = await api.patch(`/v1/admin/users/${userId}`, updatedData);
+async function updateUser(userId: string | number, data: Partial<MappedUser>) {
+  const response = await api.patch(`/v1/admin/users/${userId}`, data);
   return response.data;
 }
 
-export function useUpdateUser() {
+export const useUpdateUser = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: ({ userId, data }: { userId: number | string; data: Partial<MappedUser> }) =>
+    mutationFn: ({ userId, data }: { userId: string | number; data: Partial<MappedUser> }) =>
       updateUser(userId, data),
     onSuccess: (_, variables) => {
-      // userDetail 캐시 갱신
       queryClient.invalidateQueries({ queryKey: ["userDetail", variables.userId] });
     },
   });
-}
+};
