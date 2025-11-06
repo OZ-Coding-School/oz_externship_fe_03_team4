544a965d882f4d5129200945cca296b6996be690
kyeongbok <kyeongbok_0627@kakao.com>
2025-11-06 21:12:43 +0900

    💡 chore : 엔드포인트 확정인한 수정

--- src/api/fetchRecruitments.ts
+++ src/api/fetchRecruitments.ts
@@ -17,7 +17,7 @@ export type FetchRecruitmentsReturn = {
 export const fetchRecruitments = async (
   params: RecruitmentRequestParams // 페이지 번호, 검색어 등등
 ): Promise<FetchRecruitmentsReturn> => {
-  const url = '/v1/recruitments/admin/' // 엔드포인트 좀 더 보고 진행할게요, admin/ 이게아니라 왔다갔다함 ...
+  const url = '/v1/admin/recruitments' // 엔드포인트 좀 더 보고 진행할게요, admin/ 이게아니라 왔다갔다함 ...
   const { data } = await api.get<RecruitmentListResponseDTO>(url, { params })
   // 서버에서 받은 results 배열을 UI 형식으로 모두 변환해요.
   const items = data.results.map(
