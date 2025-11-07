6632d4274385e92ebd58f8546594c79edf5138c7
kyeongbok <kyeongbok_0627@kakao.com>
2025-11-07 20:08:11 +0900

    refactor : 구인공고관리페이지 디테일용 훅 추가

---
+++ src/hooks/recruitments/useRecruitmentDetailQuery.ts
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
+import api from '../../lib/axios'
+import {
+  mapRecruitmentDetailDTO,
+  type Recruitment,
+  type RecruitmentDetailDTO,
+  type RecruitmentDetail,
+} from '../../types/recruitments'
+
+const DETAIL_STALE_TIME_MILLISECONDS = 30 * 1000
+
+const createRecruitmentDetailQueryKey = (recruitmentId: number) =>
+  ['admin', 'recruitments', 'detail', recruitmentId] as const
+
+export const useRecruitmentDetailQuery = (recruitment: Recruitment | null) => {
+  return useQuery<RecruitmentDetail, Error>({
+    enabled: !!recruitment,
+    queryKey: recruitment
+      ? createRecruitmentDetailQueryKey(recruitment.id)
+      : ['admin', 'recruitments', 'detail', 'idle'],
+    staleTime: DETAIL_STALE_TIME_MILLISECONDS,
+    queryFn: async () => {
+      if (!recruitment) {
+        throw new Error('선택된 공고가 없습니다.')
+      }
+      const response = await api.get<RecruitmentDetailDTO>(
+        `/v1/admin/recruitments/${recruitment.id}`
+      )
+      return mapRecruitmentDetailDTO(response.data)
+    },
+  })
+}
