42e35f43999a9ad5b4c0eb61a1748718c3f43abe
danbi-seo <sdb0504@naver.com>
2025-11-11 14:14:23 +0900

    💡 chore : 강의관리 모달창, 스터디그룹 모달창에 PageHeader 추가(#145)

--- src/components/Lecture/LectureModal.tsx
+++ src/components/Lecture/LectureModal.tsx
@@ -3,6 +3,7 @@ import { CloseModalFooter } from '../modal/CloseModalFooter'
 import Modal from '../modal/Modal'
 import { ModalHeader } from '../modal/ModalHeader'
 import { LectureModalOutlet } from '../Lecture/LectureModalOutlet'
+import { BookOpen } from 'lucide-react'
 
 interface LectureModalProps {
   open: boolean
@@ -14,7 +15,12 @@ interface LectureModalProps {
 export const LectureModal = ({ open, onClose, lecture }: LectureModalProps) => {
   return (
     <Modal isOn={open} onBackgroundClick={onClose}>
-      <ModalHeader title="강의 상세 정보" onClose={onClose} />
+      <ModalHeader
+        title="강의 상세 정보"
+        onClose={onClose}
+        subtitle="LECTURE DETAIL"
+        iconComponent={BookOpen}
+      />
       <LectureModalOutlet lecture={lecture} />
       <CloseModalFooter onClose={onClose} />
     </Modal>
